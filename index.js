d5df98157e172cd2e534d23a0db73a90c2500680
HussainSaim <saimh1250@gmail.com>
2025-10-08 21:41:13 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var http = require("http");
 const port = 8000;
 
 http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello rvryonr");
-})
+    // Serve HTML so set content-type to text/html
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end("<!DOCTYPE html><head></head><body><header><h1>Welcome to My Page</h1><title>My Page</title></header><main><p>This is a simple webpage created as part of a dynamic web applications course</p><h2>About Me</h2><ul><li>Name: Saim Hussain</li><li>Age: 20</li><li>DOB: 07/11/2004</li></ul></main></body></html>");
+})        
+       
 .listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
 });
-
-DocumentTypehtml 
